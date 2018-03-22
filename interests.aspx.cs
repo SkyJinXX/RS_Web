f41ef849b7a5e9eb20e55b57df9c421b568b8722
SkyJinXX <Skyjin@outlook.com>
2018-03-22 21:38:19 +0800

    兴趣模型

--- interests.aspx.cs
+++ interests.aspx.cs
@@ -21,4 +21,13 @@ public partial class interests : System.Web.UI.Page
     {
 
     }
+
+    protected void Chart1_Load(object sender, EventArgs e)
+    {
+        List<string> xData = new List<string>() { "科技", "游戏", "体育", "汽车" ,"搞笑"};
+        List<int> yData = new List<int>() { 10, 10, 30, 40,10 };
+        Chart1.Series[0]["PieLabelStyle"] = "Outside";//将文字移到外侧
+        Chart1.Series[0]["PieLineColor"] = "Black";//绘制黑色的连线。
+        Chart1.Series[0].Points.DataBindXY(xData, yData);
+    }
 }
